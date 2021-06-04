46b2cfe809d0ca24bd530fac964c2601fc01708a
Luke Browne <luke.browne@wealthwizards.com>
2021-06-04 11:04:08 +0100

    chore: add express/dyson/supertest test harness setup

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,33 @@
-import dysonGraphQl from ".";
+import express from "express";
+import dyson from "dyson";
+import supertest from "supertest";
 
 describe("dyson-graphql", () => {
-  it("should not throw", () => {
-    expect(dysonGraphQl).not.toThrow();
+  const harness = express();
+
+  const options = {};
+  const configurations = [
+    {
+      path: "/users/:id",
+      method: "GET",
+      template: {
+        id: (params) => params.id,
+        name: "James Smith",
+      },
+    },
+  ];
+
+  dyson.registerServices(harness, options, configurations);
+
+  const agent = supertest.agent(harness);
+
+  it("should be configured correctly", async () => {
+    const { status, body } = await agent.get("/users/123");
+
+    expect(status).toBe(200);
+    expect(body).toEqual({
+      id: "123",
+      name: "James Smith",
+    });
   });
 });
